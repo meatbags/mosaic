5e59b6536414d36cd487e341a85dcfe3f0289475
meatbags <jxburrow@gmail.com>
2023-06-30 12:40:42 +1000

    v

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	  <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <script type='text/javascript' src='build/inflate.min.js'></script>
-    <script type='text/javascript' src='build/app.min.js'></script>
+    <script type='text/javascript' src='build/app.min.js?v=2023'></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
